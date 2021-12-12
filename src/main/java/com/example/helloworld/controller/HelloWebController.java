0b976d1663ac516435aa6b1901733a5b488695fa
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-12 12:11:58 +0530

    Added:

---
+++ src/main/java/com/example/helloworld/controller/HelloWebController.java
@@ -0,0 +1,20 @@
+package com.example.helloworld.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HelloWebController {
+
+    @GetMapping("/web")
+    public String hello() {
+        return "hello";
+    }
+
+
+    @GetMapping("/web/message")
+    public String message(Model model) {
+        model.addAttribute("message", "this custom message, Hello From Brdigelabz");
+        return "message";
+    }
+}
+
