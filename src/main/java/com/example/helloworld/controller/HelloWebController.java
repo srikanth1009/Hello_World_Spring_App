067a79ccaecfd624cd07924eb9d0f02109f0419d
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-12-12 12:14:33 +0530

    Added: Displaying Hello From BridgeLabs in a webpage Using MVC controller and model

--- src/main/java/com/example/helloworld/controller/HelloWebController.java
+++ src/main/java/com/example/helloworld/controller/HelloWebController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloWebController {
 
+
     @GetMapping("/web")
     public String hello() {
         return "hello";
